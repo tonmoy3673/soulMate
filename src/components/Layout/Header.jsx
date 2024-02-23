e1b0a106e51d0260cfcdea527e51a3e4a1bb7483
tonmoy <tonmoy3673@gmail.com>
2024-02-23 12:35:13 +0600

    :lipstick:polish title

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="flex px-12 py-10">
-      <div>
-        <h3>SoulMate</h3>
-      </div>
+      <Link href="/">
+        <h3 className="text-blue-500 hover:text-orange-600 text-2xl font-semibold">
+          SoulMate
+        </h3>
+      </Link>
       <ul className="mx-auto">
         <Link href="/" className="">
           Home
