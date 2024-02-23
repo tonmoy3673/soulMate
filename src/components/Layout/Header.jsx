04526a6590bcc71fd5eee4a75237f9f1e505a9cc
tonmoy <tonmoy3673@gmail.com>
2024-02-23 12:38:31 +0600

    :lipstick: polished header menu

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -9,12 +9,24 @@ const Header = () => {
         </h3>
       </Link>
       <ul className="mx-auto">
-        <Link href="/" className="">
+        <Link href="/" className="text-blue-500 hover:text-orange-600 me-4">
           Home
         </Link>
-        <Link href="/login">Login</Link>
-        <Link href="/feedback">Feedback</Link>
-        <Link href="/user">User</Link>
+        <Link
+          href="/login"
+          className="text-blue-500 hover:text-orange-600 me-4"
+        >
+          Login
+        </Link>
+        <Link
+          href="/feedback"
+          className="text-blue-500 hover:text-orange-600 me-4"
+        >
+          Feedback
+        </Link>
+        <Link href="/user" className="text-blue-500 hover:text-orange-600 me-4">
+          User
+        </Link>
       </ul>
     </div>
   );
