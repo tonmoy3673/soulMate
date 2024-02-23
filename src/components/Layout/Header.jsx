78dbb8144448fb0a322d107010e625bed2c09f43
tonmoy <tonmoy3673@gmail.com>
2024-02-23 12:29:38 +0600

    :art: align header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
-      <ul>
-        <Link href="/">Home</Link>
+    <div className="flex px-12 py-10">
+      <div>
+        <h3>SoulMate</h3>
+      </div>
+      <ul className="mx-auto">
+        <Link href="/" className="">
+          Home
+        </Link>
         <Link href="/login">Login</Link>
         <Link href="/feedback">Feedback</Link>
         <Link href="/user">User</Link>
